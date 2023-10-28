842351151741f586fe50a4dc777780fe40de9c1f
fajarutamaa <fajardwiutomo75@gmail.com>
2023-10-28 11:14:48 +0700

    add transaction feature

--- controller/transaction.controller.js
+++ controller/transaction.controller.js
@@ -3,8 +3,33 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-async function Insert(req, res){
+async function Insert(req, res) {
 
-    
+    const { source_account_id, destination_account_id, amount } = req.body
+    console.log(req.body)
 
+    const payload = {
+        source_account_id: parseInt(source_account_id),
+        destination_account_id: parseInt(destination_account_id),
+        amount: parseInt(amount),
+    }
+
+    try {
+        const transaction = await prisma.transaction.create({
+            data: payload,
+        })
+
+        let respons = ResponseFormatter(transaction, 'success', null, 200)
+        res.json(respons)
+        return
+    } catch (error) {
+        let respons = ResponseFormatter(null, 'internal server error', error, 500)
+        res.json(respons)
+        return
+    }
+}
+
+
+module.exports = {
+    Insert
 }
--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { Insert } = require('../controller/transaction.controller')
+const { CheckTransaction } = require('../middleware/middleware')
 
 
-
+router.post('/', CheckTransaction, Insert)
 
 
 module.exports = router

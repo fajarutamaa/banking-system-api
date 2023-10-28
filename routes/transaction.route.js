226ff6011fcb6989a11c30f269da3cb2220c9bc4
fajarutamaa <fajardwiutomo75@gmail.com>
2023-10-28 12:50:45 +0700

    update user controller

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -3,8 +3,6 @@ const router = express.Router()
 const { Insert } = require('../controller/transaction.controller')
 const { CheckTransaction } = require('../middleware/middleware')
 
-
 router.post('/', CheckTransaction, Insert)
-
 
 module.exports = router

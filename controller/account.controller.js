abc8dc8fa5602bb3e2186f04eb760d909d11e589
fajarutamaa <fajardwiutomo75@gmail.com>
2023-10-28 14:09:39 +0700

    update endpoint account

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -30,6 +30,66 @@ async function Insert(req, res) {
     }
 }
 
+async function GetAll(req, res) {
+
+    const { user_id, bank_name, bank_account_number } = req.query
+
+    const payload = {}
+
+    if (user_id) {
+        payload.user_id = user_id
+    }
+
+    if (bank_name) {
+        payload.bank_name = bank_name
+    }
+
+    if (bank_account_number) {
+        payload.bank_account_number = bank_account_number
+    }
+
+    try {
+        const accounts = await prisma.bankAccount.findMany({
+            where: payload,
+            orderBy: {
+                id: 'asc'
+            }
+        })
+
+        let respons = ResponseFormatter(accounts, 'success', null, 200)
+        res.json(respons)
+        return
+    } catch (error) {
+        let respons = ResponseFormatter(null, 'internal server error', error, 500)
+        res.json(respons)
+        return
+    }
+
+}
+
+async function GetById(req, res) {
+
+    const { user_id } = req.params
+
+    try {
+        const accounts = await prisma.bankAccount.findMany({
+            where: {
+                id: user_id
+            }
+        })
+
+        let respons = ResponseFormatter(accounts, 'success', null, 200)
+        res.json(respons)
+        return
+    } catch(error){
+        let respons = ResponseFormatter(null, 'internal server error', error, 500)
+        res.json(respons)
+        return
+    }
+}
+
 module.exports = {
     Insert,
+    GetAll,
+    GetById
 }
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { Insert } = require('../controller/account.controller')
-const {  CheckAccount } = require('../middleware/middleware')
+const { Insert, GetAll, GetById } = require('../controller/account.controller')
+const { CheckAccount } = require('../middleware/middleware')
 
 router.post('/', CheckAccount, Insert)
+router.get('/', GetAll)
+router.get('/:id', GetById)
 
 module.exports = router

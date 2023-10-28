736631be39241f07baedc9765e416b4c8a4948a9
fajarutamaa <fajardwiutomo75@gmail.com>
2023-10-28 13:15:04 +0700

    update endpoint profile

--- controller/profile.controller.js
+++ controller/profile.controller.js
@@ -6,12 +6,11 @@ const prisma = new PrismaClient()
 async function Insert(req, res) {
 
     const { user_id, identity_type, identity_number, address } = req.body
-    console.log(req.body)
 
     const payload = {
-        user_id : parseInt(user_id),
+        user_id: parseInt(user_id),
         identity_type,
-        identity_number : parseInt(identity_number),
+        identity_number: parseInt(identity_number),
         address,
     }
 
@@ -30,8 +29,50 @@ async function Insert(req, res) {
     }
 }
 
+async function GetAll(req, res) {
+
+    const { user_id, identity_type, identity_number, address } = req.query
+
+    const payload = {}
+
+    if (user_id) {
+        payload.user_id = user_id
+    }
+
+    if (identity_type) {
+        payload.identity_type = identity_type
+    }
+
+    if (identity_number) {
+        payload.identity_number = identity_number
+    }
+
+    if (address) {
+        payload.address = address
+    }
+
+    try {
+        const profiles = await prisma.profile.findMany({
+            where: payload,
+            orderBy: {
+                id: 'asc'
+            }
+        })
+
+        let respons = ResponseFormatter(profiles, 'success', null, 200)
+        res.json(respons)
+        return
+    } catch (error) {
+        let respons = ResponseFormatter(null, 'internal server error', error, 500)
+        req.json(respons)
+        return
+    }
+
+}
+
 
 
 module.exports = {
     Insert,
+    GetAll
 }
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { Insert } = require('../controller/profile.controller')
+const { Insert, GetAll } = require('../controller/profile.controller')
 const { CheckProfile } = require('../middleware/middleware')
 
 router.post('/', CheckProfile, Insert)
+router.get('/', GetAll)
 
 module.exports = router

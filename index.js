3eda7dbfefae13d2f64de0df29a076950bc5ba06
fajarutamaa <fajardwiutomo75@gmail.com>
2023-10-31 20:36:57 +0700

    add documentation swagger

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/route')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJSDoc = require('swagger-jsdoc')
 
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
 
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API',
+      version: '1.0.0',
+      description:
+        'This is a REST API application made with Express',
+      license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'Fajar Dwi Utomo',
+        url: 'fajardwiutomo75@gmail.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:8080',
+        description: 'Development server',
+      },
+    ],
+  }
+  
+  const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes/user.route.js'],
+  }
+  
+  const swaggerSpec = swaggerJSDoc(options)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 app.use('/API/v1/', router)
 
 app.listen(port, () => {
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,9 +4,45 @@ const { Insert, GetById, Delete, Update, GetAll } = require('../controller/user.
 const { CheckProcess } = require('../middleware/middleware')
 
 router.post('/', CheckProcess, Insert)
+
+/**
+ * @swagger
+ * /API/v1/user/{id}:
+ *   get:
+ *     tags : 
+ *      - "user"
+ *     summary: example to get user by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: success
+ *       500:
+ *         description: internal server error
+ */
 router.get('/:id', GetById)
+
 router.put('/:id', Update)
 router.delete('/:id', Delete)
+
+/**
+ * @swagger
+ * /API/v1/user:
+ *   get:
+ *     tags : 
+ *      - "user"
+ *     summary: example to get user
+ *     responses:
+ *       200:
+ *         description: success
+ *       500:
+ *         description: internal server error
+ */
 router.get('/', GetAll)
 
 
